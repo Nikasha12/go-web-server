5d1f38bd175df1029ae5686884c4b6621247fd6e
Nikasha <nikashamishra4@gmail.com>
2023-10-12 19:58:09 -0400

    update server file

--- server.go
+++ server.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-	})
-
+	http.HandleFunc("/", servePage)
 	http.ListenAndServe(":8080", nil)
+}
+
+func servePage(writer http.ResponseWriter, reqest *http.Request) {
+	io.WriteString(writer, "Hello world!")
 }
